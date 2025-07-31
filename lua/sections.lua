a1c6ff8c476faf8b5d395e9bfca6c5b22982e449
theogilbert <theovgilbert@gmail.com>
2025-07-31 20:15:16 +0200

    refresh sections on save and win switch

--- lua/sections.lua
+++ lua/sections.lua
@@ -3,12 +3,45 @@ local pane = require("sections.pane")
 
 local M = {}
 
+local function setup_autocommands()
+    local group = vim.api.nvim_create_augroup("SectionsAutoRefresh", { clear = true })
+
+    -- When saving a file, refresh pane if the file is currently watched
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = group,
+        callback = function(args)
+            if args.buf == pane.get_watched_buf() then
+                pane.refresh_pane(args.buf)
+            end
+        end,
+    })
+    -- When entering a window, if it's not
+    vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+        group = group,
+        callback = function(args)
+            if args.buf ~= pane.get_pane_buf() then
+                local win = vim.api.nvim_get_current_win()
+                pane.refresh_pane(args.buf, win)
+            end
+        end,
+    })
+    vim.api.nvim_create_autocmd("WinClosed", {
+        group = group,
+        callback = function(args)
+            if args.buf == pane.get_pane_buf() then
+                pane.cleanup_pane()
+            end
+        end,
+    })
+end
+
 M.toggle = function()
     pane.toggle_pane()
 end
 
 M.setup = function(config)
     init_config(config)
+    setup_autocommands()
 end
 
 return M
--- lua/sections/pane.lua
+++ lua/sections/pane.lua
@@ -1,73 +1,53 @@
-local M = {}
-
-local PANE_FILETYPE = "sections-pane"
-local _source_win = nil
-
+-- TODO add colored prefix icon depending on capture type
+-- TODO parse public/private functions
 local parser = require("sections.parser")
 local formatter = require("sections.formatter")
 
+local M = {}
 
-local function detect_pane_width(lines, min, max)
-    min = min or 20
-    max = max or 100
+local PANE_FILETYPE = "sections-pane"
+local _pane_infos = {}
 
-    local width = min
-
-    for _, line in pairs(lines) do
-        if #line > width then
-            width = #line + 2
-        end
-    end
-
-    if width > max then
-        width = max
-    end
-
-    return width
+local function get_pane_info()
+    local cur_tab = vim.api.nvim_get_current_tabpage()
+    return _pane_infos[cur_tab]
 end
 
-local function notify_error(msg)
-    vim.notify(msg, vim.log.levels.ERROR)
-end
-
-local function select_section()
-    if _source_win == nil or not vim.api.nvim_win_is_valid(_source_win) then
-        notify_error("Source window not found")
-        return
-    end
+local function on_section_selected()
+    local info = get_pane_info()
 
     local cur_line = vim.api.nvim_win_get_cursor(0)[1]
     local pos = formatter.get_section_pos(cur_line)
 
-    vim.api.nvim_win_set_cursor(_source_win, pos)
+    if pos ~= nil then
+        vim.api.nvim_win_set_cursor(info.watched_win, pos)
+    end
 end
 
 
-local function open_pane(src_buf)
-    _source_win = vim.api.nvim_get_current_win()
-
+local function open_pane()
     local bufid = vim.api.nvim_create_buf(true, false)
     vim.bo[bufid].filetype = PANE_FILETYPE
     vim.bo[bufid].buftype = "nofile"
-
-    local sections, err = parser.parse_sections(src_buf)
-    if sections == nil and err ~= nil then
-        notify_error(err)
-        return
-    end
-    formatter.update_sections(sections)
-    local lines = formatter.format()
-    vim.api.nvim_buf_set_lines(bufid, 0, -1, false, lines)
-
-    local width = detect_pane_width(lines)
+    vim.bo[bufid].modifiable = false
 
     local winid = vim.api.nvim_open_win(
-        bufid, false, { vertical = true, split = "left", width = width, style = "minimal"}
+        bufid, false, { vertical = true, split = "left", width = 50, style = "minimal"}
     )
     vim.api.nvim_set_option_value("cursorline", true, { win = winid })
-    vim.keymap.set("n", "<cr>", select_section, { buffer = bufid })
+    vim.keymap.set("n", "<cr>", on_section_selected, { buffer = bufid })
 
-    vim.bo[bufid].modifiable = false
+    local tab = vim.api.nvim_get_current_tabpage()
+    local watched_buf = vim.api.nvim_get_current_buf()
+    local watched_win = vim.api.nvim_get_current_win()
+    _pane_infos[tab] = {
+        pane_buf = bufid,
+        pane_win = winid,
+        watched_buf = watched_buf,
+        watched_win = watched_win,
+    }
+
+    M.refresh_pane(watched_buf)
 end
 
 local function close_pane(winid)
@@ -75,27 +55,76 @@ local function close_pane(winid)
     vim.api.nvim_buf_delete(bufid, {force = true})
 end
 
-local function get_pane_win_id()
-    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-        local bufid = vim.api.nvim_win_get_buf(winid)
 
-        if vim.bo[bufid].filetype == PANE_FILETYPE then
-            return winid
-        end
+M.cleanup_pane = function()
+    local cur_tab = vim.api.nvim_get_current_tabpage()
+    _pane_infos[cur_tab] = nil
+end
+
+M.get_watched_buf = function()
+    local info = get_pane_info()
+    if info == nil then
+        return nil
     end
 
-    return nil
+    return info.watched_buf
+end
+
+M.get_pane_buf = function()
+    local info = get_pane_info()
+    if info == nil then
+        return nil
+    end
+
+    return info.pane_buf
 end
 
 M.toggle_pane = function()
-    local win_id = get_pane_win_id()
-    local src_buf = vim.api.nvim_get_current_buf()
+    local pane_info = get_pane_info()
 
-    if win_id ~= nil then
-        close_pane(win_id)
+    if pane_info ~= nil then
+        close_pane(pane_info.pane_win)
     else
-        open_pane(src_buf)
+        open_pane()
     end
 end
+
+M.refresh_pane = function(updated_buf, buf_win)
+    if updated_buf == nil then
+        return
+    end
+
+    local pane_info = get_pane_info()
+    if pane_info == nil then
+        return
+    end
+
+    if buf_win ~= nil then
+        local win_cfg = vim.api.nvim_win_get_config(buf_win)
+        if win_cfg.relative ~= "" then
+            return  -- Window is floating
+        end
+    end
+
+    local sections, err = parser.parse_sections(updated_buf)
+    local lines = {}
+    if sections ~= nil then
+        formatter.update_sections(sections)
+        lines = formatter.format()
+    elseif err ~= nil then
+        lines = { err }
+    end
+
+    local pane_buf = pane_info.pane_buf
+    vim.bo[pane_buf].modifiable = true
+    vim.api.nvim_buf_set_lines(pane_buf, 0, -1, false, lines)
+    vim.bo[pane_buf].modifiable = false
+
+    pane_info.watched_buf = updated_buf
+    if buf_win ~= nil then
+        pane_info.watched_win = buf_win
+    end
+end
+
 
 return M
